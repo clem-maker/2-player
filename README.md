4a0088f782755e0bfb476ed50e22441246ed32a9
clem-maker <clemenshillger12@gmail.com>
2026-08-23 20:12:56 +0200

    stuff

---
+++ README.md
@@ -0,0 +1,5 @@
+#Projekt Idea
+I want to have a platformer where you can chase each other but the catch your jump button is connected
+I am not good at handeling online multiplayer so I want to make it a local multiplayer.
+
+Pixel art is created via Asesprit.
--- scribte/globals/JUMP_MANAGER.gd
+++ scribte/globals/JUMP_MANAGER.gd
@@ -7,7 +7,7 @@ var visited : int = 0
 
 func count_time(delta:float) -> float:
 	jump_time += delta /2 #if both players acess this function then it growths twise as fast as intendet so you need to devide it by two.
-	return jump_time 
+	return jump_time *1.05
 
 
 
@@ -20,6 +20,5 @@ func reset_jumptime():
 	#if both have than reset zero so the values don't stack.
 	if visited >= 2:
 		jump_time = 0
-		print(jump_time)
 		#don't forget to reset the value:
 		visited = 0
