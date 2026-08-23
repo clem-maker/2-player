24c80dc26ad7bbc9783de776866c67fc3d02ec2b
clem-maker <clemenshillger12@gmail.com>
2026-08-23 16:19:41 +0200

    stuff

--- scribte/player_rot.gd
+++ scribte/player_rot.gd
@@ -1,14 +1,11 @@
 extends CharacterBody2D
-
-
 const SPEED = 700
 const JUMP_VELOCITY = -650.0
+
 var multiplier
 
-
-
-
 func _physics_process(delta: float) -> void:
+	#gravity
 	if not is_on_floor():
 		if velocity.y > 0.0:
 			multiplier = 2.2
@@ -20,10 +17,11 @@ func _physics_process(delta: float) -> void:
 	#jumpen
 	if Input.is_action_pressed("jump") and is_on_floor():
 		JumpManager.jump_time = JumpManager.count_time(delta) #debug jump :   print(jump_time)
+		print(JumpManager.jump_time)
 	if Input.is_action_just_released("jump"):
 		var clamped_jump_time = clampf( JumpManager.jump_time , 0., JumpManager.max_jump_height)
 		velocity.y = clamped_jump_time * JUMP_VELOCITY
-		JumpManager.jump_time = 0.0
+		
 
 
 	var direction := Input.get_axis("left_arrow","right_arrow")
@@ -34,3 +32,4 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	move_and_slide()
+	JumpManager.jump_time = 0.0
