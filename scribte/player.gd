1e0dc790cceb874f390454a7c7be6feb4c13953a
clem-maker <clemenshillger12@gmail.com>
2026-08-25 18:04:34 +0200

    state_machine start
    my first try of a state machine but i have a error i can't jump high enough it kinda clamped to a low value.

--- scribte/player.gd
+++ scribte/player.gd
@@ -7,12 +7,16 @@ const max_jump_height : float = 1.5
 
 var multiplier
 var jump_time :float = 0
-var direction
+var direction : float = 0
 
 #state machine:
 const states_avalibile : Array = ["default" , "jumping"]
 var state = "default"
 
+func get_state()->int:
+	if state == "defualt": return 0
+	elif state == "jumping": return 1
+	else: return -1 # ERROR
 func get_direction():
 	direction = Input.get_axis("left","right")
 	return direction
@@ -20,34 +24,59 @@ func count_time(delta:float) -> float:
 	jump_time += delta /2 #if both players acess this function then it growths twise as fast as intendet so you need to devide it by two.
 	print(jump_time)
 	return jump_time 
+func is_current_state_correct(current_state :String) ->int:
+	if is_on_floor() and not current_state == states_avalibile[0]:
+		current_state = states_avalibile[0] #if player is on floor and the state is not equal to states_Avalabile[0],
+		#                                    ("default") than change the variable states to "default", LOWERCASE!
 
+	elif not is_on_floor() and not current_state == states_avalibile[1]:
+		current_state = states_avalibile[1]#if player is on floor and the state is not equal to states_Avalabile[1],
+		#                                    ("jumping") than change the variable states to "default", LOWERCASE!
+	
+	if current_state == "default": return 0
+	elif current_state =="jumping": return 1
+	else: return -1 #Error handeling
+func change_state_to(new_state : String) ->void:
+	if state in states_avalibile:
+		state = new_state
+		print(state)
+		return
+	print_debug("Fehler state not avalible")
 func _input(_event: InputEvent) -> void:
-	
-	#jumpen
-	var delta = get_process_delta_time()
-	if Input.is_action_pressed("jump") and is_on_floor():
-		jump_time = count_time(delta) #debug jump :   print(jump_time)
-	if Input.is_action_just_released("jump"):
-		var clamped_jump_time = clampf( jump_time , 0.5, max_jump_height)
-		velocity.y = clamped_jump_time * JUMP_VELOCITY
-		jump_time = 0
-	
-	#moving (right/left)
-	direction = get_direction() #Input.get_axis("left","right")
-func _physics_process(delta: float) -> void:
-	#gravity
-	if not is_on_floor():
-		if velocity.y > 0.0:
-			multiplier = 2.2
-		else:
-			multiplier = 1.0
-		velocity += get_gravity() * multiplier * delta
+	if state in states_avalibile:# always
+		#moving (right/left)
+		direction = get_direction() #Input.get_axis("left","right")
 		
-	#moving right/left
-	velocity.x = direction * SPEED
-	#smoth deaccleration
-	if direction != 0:
-		velocity.x = direction * SPEED #normel movment
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED) #if no input
+		#cheak if Input spacebutton is equal to jump, if true the state will be set to jumping:
+		if Input.is_action_just_released("jump") and not get_state() == 1: # if jumpbutton and state is not allready set to jump (so you don't make a dpuble jump or jump in the air ) 
+			change_state_to(states_avalibile[1]) #state is set to jump
+			
+	
+	if state == states_avalibile[1]: #jumping
+		#jumpen
+		var delta = get_process_delta_time()
+		if Input.is_action_pressed("jump") and is_on_floor():
+			jump_time = count_time(delta) #debug jump :   print(jump_time)
+		if Input.is_action_just_released("jump"):
+			var clamped_jump_time = clampf( jump_time , 0.5, max_jump_height)
+			velocity.y = clamped_jump_time * JUMP_VELOCITY
+			jump_time = 0
+func _physics_process(delta: float) -> void:
+	state = states_avalibile[is_current_state_correct(state)] #debug states here:    print("curren state is:",is_current_state_correct(state))
+	if state in states_avalibile: #immer
+		#gravity
+		if not is_on_floor():
+			if velocity.y > 0.0:
+				multiplier = 2.2
+			else:
+				multiplier = 1.0
+			velocity += get_gravity() * multiplier * delta
+			
+		#moving right/left
+		velocity.x = direction * SPEED
+		#smoth deaccleration
+		if direction != 0:
+			velocity.x = direction * SPEED #normel movment
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED) #if no input
 	move_and_slide()
--- scribte/player_rot.gd
+++ scribte/player_rot.gd
@@ -1,5 +1,5 @@
 extends player_scribt
 
 func get_direction():
-	var direction = Input.get_axis("left_arrow","right_arrow") #arrow keys
+	direction = Input.get_axis("left_arrow","right_arrow") #arrow keys
 	return direction
