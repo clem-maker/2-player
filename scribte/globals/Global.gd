c2c3f8fe8aa5c5f7ad4d01cd423600239ef64fe7
clem-maker <clemenshillger12@gmail.com>
2026-09-07 16:40:10 +0200

    stuff

--- scribte/globals/Global.gd
+++ scribte/globals/Global.gd
@@ -6,3 +6,5 @@ func _input(_event: InputEvent) -> void:
 		get_tree().quit(0)
 	if Input.is_action_just_pressed("restart_game"):
 		SceneManager.change_scene_to("main")
+	if Input.is_action_just_pressed("fullscreen"):
+		Window.MODE_FULLSCREEN
