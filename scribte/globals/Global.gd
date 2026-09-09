2cd25ece47a2dd41c5285b235f89775fc64e53c9
clem-maker <clemenshillger12@gmail.com>
2026-09-09 18:58:28 +0200

    stuff

--- scribte/globals/Global.gd
+++ scribte/globals/Global.gd
@@ -4,7 +4,10 @@ extends Node
 func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit(0)
+		print("closing game...")
 	if Input.is_action_just_pressed("restart_game"):
 		SceneManager.change_scene_to("main")
+		print("restarting game")
 	if Input.is_action_just_pressed("fullscreen"):
-		Window.MODE_FULLSCREEN
+		# Dies ist der korrekte Befehl:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
