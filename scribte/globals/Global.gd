38a8b93f737dcf79d17e96cf7360f7b05c3ec962
clem-maker <clemenshillger12@gmail.com>
2026-09-11 18:55:03 +0200

    fix
    you can now change back from exclusive  fullscreen to window mode

--- scribte/globals/Global.gd
+++ scribte/globals/Global.gd
@@ -8,6 +8,10 @@ func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("restart_game"):
 		SceneManager.change_scene_to("main")
 		print("restarting game")
+	#fullscreen
 	if Input.is_action_just_pressed("fullscreen"):
-		# Dies ist der korrekte Befehl:
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+		
+		if DisplayServer.window_get_mode() == 0:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+		else:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
